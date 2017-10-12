ef7b932d7e7916a69bbb75f59e85c62daca952ae
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-11 21:43:15 -0500

    specs for friendship callbacks

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,22 +3,22 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: "Member"
 
   after_create :create_inverse, unless: :has_inverse?
-  after_destroy :destroy_inverses, if: :has_inverse?
+  after_destroy :destroy_inverse, if: :has_inverse?
 
   def create_inverse
     Friendship.create(inverse_params)
   end
 
-  def destroy_inverses
-    inverses.destroy
+  def destroy_inverse
+    inverse.destroy
   end
 
   def has_inverse?
     Friendship.exists?(inverse_params)
   end
 
-  def inverses
-    Friendship.where(inverse_params)
+  def inverse
+    Friendship.where(inverse_params)[0]
   end
 
   def inverse_params
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Friendship, type: :model do
+  let(:member1) { create(:member) }
+  let(:member2) { create(:member) }
+  let(:friendship)       { Friendship.new(member_id: member1.id, friend_id: member2.id) }
+
+  describe "create friendship creates inverse friendship" do
+    it "adds two friendships" do
+      expect{ friendship.save }.to change{ Friendship.all.size }.by(2)
+    end
+
+    it "inverse relationship created" do
+      friendship.save
+      expect(Friendship.where({ member_id: friendship.friend_id, friend_id: friendship.member_id }).exists?).to be_truthy
+    end
+  end
+
+  describe "destroy friendship destroys inverse friendship" do
+    it "removes two friendships" do
+      friendship.save
+      expect{ Friendship.destroy(friendship.id) }.to change{ Friendship.all.size }.by(-2)
+    end
+
+    it "inverse relationship destroyed" do
+      friendship.save
+      Friendship.destroy(friendship.id)
+      expect(Friendship.where({ member_id: friendship.friend_id, friend_id: friendship.member_id }).exists?).to be_falsey
+    end
+  end
 end
