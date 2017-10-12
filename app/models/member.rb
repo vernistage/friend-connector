37b2a1bc3a40ae4e1be18eeb6c94f29e82f471ca
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-11 21:13:35 -0500

    change HABTM relationship to has many

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id              :integer          not null, primary key
+#  name            :string           not null
+#  website         :string           not null
+#  short_url       :string           default(""), not null
+#  scraped_content :text             default(""), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Member < ApplicationRecord
+  has_many :friendships
+  has_many :friends, through: :friendships
 end
