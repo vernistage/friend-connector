21b3542b348a1c93c5da225f0a4d8015f3df659f
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-12 02:22:37 -0500

    update annotations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  member_id  :integer
+#  friend_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Friendship < ApplicationRecord
   belongs_to :member
   belongs_to :friend, class_name: "Member"
--- spec/factories/friendships.rb
+++ spec/factories/friendships.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  member_id  :integer
+#  friend_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :friendship do
     member_id 1
