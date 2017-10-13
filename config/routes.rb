40bfb345fc3e096f6269fb416aae10d399bea71a
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-13 13:04:38 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "members/member_results", to: 'members#member_results'
   resources :members, only: [:new, :show, :index, :create]
   resources :friendships, only: [:create]
+
+  root 'members#new'
 end
