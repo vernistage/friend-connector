8043cb428a58e9e88297217e8e0cd6e617c5730c
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-13 01:01:26 -0500

    finish algorithm

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,11 @@
 class FriendshipsController < ApplicationController
-  
+
   def create
-    @friendship = Friendship.new(friendship_params)
-    @friend = Member.find(friendship_params[:friend_id])
-    @befriender = Member.find(friendship_params[:member_id])
+    @friendship = Friendship.new(f_params)
+    @friend = Member.find(f_params[:friend_id])
+    @befriender = Member.find(f_params[:member_id])
     if (!@friendship.same_person?(@befriender.id, @friend.id) && @friendship.save)
-      flash[:notice] = "You are now friends with #{@friend.name}"
+      flash[:notice] = "#{@befriender.name} is now friends with #{@friend.name}"
       redirect_to @befriender
     else
       flash[:alert] = "Could not connect to member"
@@ -14,7 +14,7 @@ class FriendshipsController < ApplicationController
   end
 
   private
-  def friendship_params
+  def f_params
     params.require(:friendship).permit(:member_id, :friend_id)
   end
 end
--- app/services/friend_connections_service.rb
+++ app/services/friend_connections_service.rb
@@ -1,28 +1,21 @@
 class FriendConnectionsService
 
-  def find_pathway(poss_friends, member, friend)
+  def find_pathway(poss_friends, member, possible_friend)
     queue = Queue.new
-    friend_pathway = [member]
-    queue.enq(friend_pathway)
-
-  while(!queue.empty?)
-    current_path = queue.deq
-    #current node is last in queue
-    current_node_member = current_path[current_path.length-1]
-    #until we find the possible friend
-    until current_node_member == possible_friend
-      # if current node has friends
-      if current_node_member.friends.any?
-        #add current node to friend pathway
-        friend_pathway << current_node_member
-        # go through each friend
-        current_node_member.friends.each do |friend|
-          # add friend to queue
-          queue.enq(friend)
+    friend_path = [member]
+    new_path = Array.new
+    queue.enq(friend_path)
+    while(!queue.empty?)
+      current_path = queue.deq
+      current_node_member = current_path[current_path.length-1]
+      break if current_node_member == possible_friend
+      current_node_member.friends.each do |friend|
+        if !current_path.include?(friend)
+          new_path = friend_path + [friend]
+          queue.enq(new_path)
         end
       end
     end
+    new_path << possible_friend
   end
-  # return final pathway
-  friend_pathway
 end
