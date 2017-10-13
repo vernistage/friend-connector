a4a072d97a3b218a46e896890ba7caa4ade79927
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-12 19:35:12 -0500

    service methods

--- app/services/friend_finder_service.rb
+++ app/services/friend_finder_service.rb
@@ -1,8 +1,20 @@
 class FriendFinderService
 
-  def initialize(mem_id, topic)
-    @befriender_id = mem_id
-    @topic = topic
+  def initialize(id, topic)
+    @member = Member.find(id)
+    @topic = topic.downcase!
   end
-  
+
+  def find_friends
+    specialists(possible_new_friends(@member))
+  end
+
+  def possible_new_friends(member)
+    Member.all - member.friends
+  end
+
+  def specialists(friends_arr)
+    topic = "%#{@topic}%"
+    Member.where("scraped_content LIKE ?", topic)
+  end
 end
