8334d55faf9e3c76d064d6c906bdebc400832c7e
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-10-12 20:30:11 -0500

    configure cov gem for test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'support/factory_girl'
+require 'simplecov'
+SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
